edf4afd852378589561920c8b67ccb7454771995
Orlando Curieles <orlando.curieles@ingeint.com>
2022-09-26 13:28:40 -0400

    Fix CreditNote

--- utils/fiscal.py
+++ utils/fiscal.py
@@ -126,22 +126,49 @@ def create_credit_note(self):
         f.write("SUB-TOTAL:".ljust(22) + str(self['sub_total']) + "\n")
         f.write("DESCUENTO:".ljust(24) + str(self['discount']) + "\n")
         f.write("TOTAL A PAGAR".ljust(22) + str(self['amount_total']) + "\n")
-        f.write("EFECTIVO:".ljust(22) + str(self['amount_total']) + "\n")
-        f.write("CHEQUES".ljust(22) + str(0.0) + "\n")
-        f.write("TARJ / DEBITO:".ljust(22) + str(0.0) + "\n")
-        f.write("TARJ / CREDITO:".ljust(22) + str(0.0) + "\n")
-        f.write("Tranf en Bs:".ljust(22) + str(0.0) + "\n")
-        f.write("Transf en USD:".ljust(22) + str(0.0) + "\n")
-        f.write("Transf EURO:".ljust(22) + str(0.0) + "\n")
-        f.write("Efect USD:".ljust(22) + str(0.0) + "\n")
-        f.write("Efect EURO:".ljust(22) + str(0.0) + "\n")
-        f.write("Pago movil:".ljust(22) + str(0.0) + "\n")
-        f.write("CREDITO:".ljust(22) + str(0.0) + "\n")
+
+        payments = self['payment_methods']
+
+        for payment in payments:
+            if '01' in payment:
+                f.write("EFECTIVO:".ljust(22) + str(payment['01']) + "\n")
+                continue
+            if '02' in payment:
+                f.write("CHEQUES".ljust(22) + str(payment['02']) + "\n")
+                continue
+            if '03' in payment:
+                f.write("TARJ / DEBITO:".ljust(22) + str(payment['03']) + "\n")
+                continue
+            if '04' in payment:
+                f.write("TARJ / CREDITO:".ljust(22) + str(payment['04']) + "\n")
+                continue
+            if '05' in payment:
+                f.write("Tranf en Bs:".ljust(22) + str(payment['05']) + "\n")
+                continue
+            if '06' in payment:
+                f.write("Transf en USD".ljust(22) + str(payment['06']) + "\n")
+                continue
+            if '07' in payment:
+                f.write("Pago IGTF TRF:".ljust(22) + str(payment['07']) + "\n")
+                continue
+            if '08' in payment:
+                f.write("Efect USD:".ljust(22) + str(payment['08']) + "\n")
+                continue
+            if '09' in payment:
+                f.write("Pago IGTF EFE:".ljust(22) + str(payment['09']) + "\n")
+                continue
+            if '10' in payment:
+                f.write("Pago movil:".ljust(22) + str(payment['10']) + "\n")
+                continue
+            if '11' in payment:
+                f.write("CREDITO:".ljust(22) + str(payment['11']) + "\n")
+                continue
+
         f.write("NOTA 1:" .ljust(7) + "\n")
         f.write("NOTA 2:".ljust(7) + "\n")
         f.write("NOTA 3:".ljust(7) + "\n")
         f.write("NOTA 4:".ljust(7) + "\n")
-        f.write("FACTURAAFECTADA:      " + self['invoice_affected'] + "\n")
+        f.write("FACTURAAFECTADA:      " + self['invoice_affected'])
 
         f.close()
         file_destination = 'C:/FACTURAS/' + self['file_name']
